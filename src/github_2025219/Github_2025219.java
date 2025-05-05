bc42f8141d07537ee66cc4b9e44d1d8aaaf322e3
Erdenezaya219 <erdenezayaerdenebulgan@gmail.com>
2025-05-05 22:43:56 +0000

    a message was added

--- src/github_2025219/Github_2025219.java
+++ src/github_2025219/Github_2025219.java
@@ -15,6 +15,9 @@ public class Github_2025219 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       System.out.println("Erdenezaya Erdenebulgan 2025219");
+        
     }
     
 }
